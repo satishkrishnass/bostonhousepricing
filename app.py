7da30e71bbf952ee31b4906bae7fb1502658a9b2
Satish <satishkrishnasama@gmail.com>
2022-11-12 15:54:49 +0530

    third commit - addding Procfile for heroku

--- app.py
+++ app.py
@@ -21,5 +21,13 @@ def predict_api():
     predicted_output    = xgmodel.predict(data_after_scaling)
     return jsonify(float(predicted_output[0]))
 
+@app.route('/predict',methods=['POST'])
+def predict():
+    data=[float(x) for x in request.form.values()]
+    final_input=scaler.transform(np.array(data).reshape(1,-1))
+    print(final_input)
+    output=xgmodel.predict(final_input)[0]
+    return render_template("home.html",prediction_text="The House price prediction is {}".format(output))
+
 if __name__=="__main__":
     app.run(debug=True)
